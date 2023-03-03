03b665d7e4b040e48704f568882f0709a85dcd42
hnt224 <hnt224@lehigh.edu>
2023-03-03 18:51:58 -0500

    used maven to create project template

---
+++ admin-cli/src/main/java/edu/lehigh/cse216/team_m/admin/App.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse216.team_m.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
