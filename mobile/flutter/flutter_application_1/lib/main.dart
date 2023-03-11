6e59832d6d47ff6169a0bb6d7a678d391be55491
David-CN <2245076558@qq.com>
2023-03-11 07:37:10 -0500

    update mobile

--- mobile/flutter/flutter_application_1/lib/main.dart
+++ mobile/flutter/flutter_application_1/lib/main.dart
@@ -4,32 +4,58 @@ import 'dart:developer' as developer;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+
 void main() {
   runApp(const MyApp());
 }
 
-/// Create object from data like: http://www.cse.lehigh.edu/~spear/5k.json
-class NumberWordPair {
-  /// The string representation of the number
-  final String str; 
-  /// The int representation of the number
-  final int num;
 
-  const NumberWordPair({
-    required this.str,
-    required this.num,
+/// Create object from data like: {mId: 124, mSubject: testing dokku, mMessage: please work, mLikes: 0, mCreated: Mar 10, 2023 8:10:10 PM}
+class mLine {
+  final int mId;
+  final String mSubject;
+  String mMessage;
+  int mLikes;
+  final String mCreated;
+  mLine({
+    required this.mId,
+    required this.mSubject,
+    required this.mMessage,
+    required this.mLikes,
+    required this.mCreated,
   });
 
-  factory NumberWordPair.fromJson(Map<String, dynamic> json) {
-    return NumberWordPair(
-      str: json['str'],
-      num: json['num'],
+
+  factory mLine.fromJson(Map<String, dynamic> json) {
+    return mLine(
+      mId: json['mId'],
+      mSubject: json['mSubject'],
+      mMessage: json['mMessage'],
+      mLikes: json['mLikes'],
+      mCreated: json['mCreated'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['mStatus'] = 'ok';
+    data['mData'] = {
+      'mId': this.mId,
+      'mSubject': this.mSubject,
+      'mMessage': this.mMessage,
+      'mLikes': this.mLikes,
+      'mCreated': this.mCreated
+    };
+    print(jsonEncode(data));
+    return data;
   }
 }
 
+//{"mStatus":"ok","mData":{"mId":3,"mSubject":"Test Test","mMessage":"Hello","mLikes":9,"mCreated":"Mar 11, 2023 2:37:31 AM"}}
+//{"mStatus":"ok","mData":{"mId":3,"mSubject":"Test Test","mMessage":"Hello","mLikes":9,"mCreated":"Mar 11, 2023 2:41:02 AM"}}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
@@ -46,35 +72,41 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.brown,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Team M Flutter App'),
     );
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
+
 
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
+
   final String title;
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    
+   
     // This method is rerun every time setState is called
     //
     // The Flutter framework has been optimized to make rerunning build methods
@@ -95,58 +127,80 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+
 class HttpReqWords extends StatefulWidget {
   const HttpReqWords({Key? key}) : super(key: key);
+
 
   @override
   State<HttpReqWords> createState() => _HttpReqWordsState();
 }
 
+
 class _HttpReqWordsState extends State<HttpReqWords> {
-  late Future<List<NumberWordPair>> _future_list_numword_pairs;
+  late Future<List<mLine>> _future_list_numword_pairs;
+
 
   final _biggerFont = const TextStyle(fontSize: 18);
+
 
   @override
   void initState() {
     super.initState();
-    _future_list_numword_pairs = fetchNumberWordPairs();
+    _future_list_numword_pairs = fetchmLines();
   }
+
 
   void _retry() {
     setState(() {
-      _future_list_numword_pairs = fetchNumberWordPairs();
+      _future_list_numword_pairs = fetchmLines();
     });
   }
 
+
+  void reload() {
+    setState(() {
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    var fb = FutureBuilder<List<NumberWordPair>>(
+    var fb = FutureBuilder<List<mLine>>(
       future: _future_list_numword_pairs,
-      builder: (BuildContext context, AsyncSnapshot<List<NumberWordPair>> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<List<mLine>> snapshot) {
         Widget child;
+
 
         if (snapshot.hasData) {
           // developer.log('`using` ${snapshot.data}', name: 'my.app.category');
           // create  listview to show one row per array element of json response
           child = ListView.builder(
-              //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
-              padding: const EdgeInsets.all(16.0),
-              itemCount: snapshot.data!.length,
-              itemBuilder: /*1*/ (context, i) {
-                return Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text(
-                        "row ${i}: num=${snapshot.data![i].num} str=${snapshot.data![i].str}",
-                        // snapshot.data![i].str,
-                        style: _biggerFont,
-                      ),
+            padding: const EdgeInsets.all(16.0),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, i) {
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      "id=${snapshot.data![i].mId} | ${snapshot.data![i].mMessage} | ${snapshot.data![i].mLikes}",
+                      style: _biggerFont,
                     ),
-                    Divider(height: 1.0),
-                  ],
-                );
-              });
+                    trailing: ElevatedButton(
+                      onPressed: () {
+                        int newLikes = snapshot.data![i].mLikes + 1;
+                        updateLikes(snapshot.data![i], snapshot.data![i].mId, newLikes);
+                        //snapshot.data![i].mLikes++;
+                        _retry();
+                      },
+                      child: Text('Like'),
+                    ),
+                  ),
+                  Divider(height: 1.0),
+                ],
+              );
+            },
+          );
         } else if (snapshot.hasError) { // newly added
           child = Text('${snapshot.error}');
         } else {
@@ -158,64 +212,23 @@ class _HttpReqWordsState extends State<HttpReqWords> {
       },
     );
 
+
     return fb;
   }
 }
 
-// method for trying out a long-running calculation
-Future<List<String>> doSomeLongRunningCalculation() async {
-  
-  return getWebData(); 
-}
-
-Future<List<String>> simpleLongRunningCalculation() async {
-  await Future.delayed(Duration(seconds: 5)); // wait 5 sec
-  return ['x', 'y', 'z'];
-}
-
-Future<List<String>> getWebData() async {
-  developer.log('Making web request...');
-  // var url = Uri.http('www.cse.lehigh.edu', '~spear/courses.json');
-  // var url = Uri.parse('http://www.cse.lehigh.edu/~spear/courses.json'); // list of strings
-  var url = Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json');      // list of maps
-  // var url = Uri.parse('https://jsonplaceholder.typicode.com/albums/1'); // single map
-  var headers = {"Accept": "application/json"};  // <String,String>{};
-
-  var response = await http.get(url, headers: headers);
-
-  developer.log('Response status: ${response.statusCode}');
-  developer.log('Response headers: ${response.headers}');
-  developer.log('Response body: ${response.body}');
-
-  var res = jsonDecode(response.body);
-  print('json decode: $res');
-
-  final List<String> returnData;
-  if( res is List ){
-    returnData = (res as List<dynamic>).map( (x) => x.toString() ).toList();
-  }else if( res is Map ){
-    returnData = <String>[(res as Map<String,dynamic>).toString()];
-  }else{
-    developer.log('ERROR: Unexpected json response type (was not a List or Map).');
-    returnData = List.empty();
-  }
-
-  return returnData;
-}
-
-Future<List<NumberWordPair>> fetchNumberWordPairs() async {
+Future<List<mLine>> fetchmLines() async {
   final response = await http
-      .get(Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json'));
+      .get(Uri.parse('http://2023sp-team-m.dokku.cse.lehigh.edu/messages'));
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response, then parse the JSON.
-    final List<NumberWordPair> returnData;
+    final List<mLine> returnData;
     var res = jsonDecode(response.body);
-    print('json decode: $res');
-
-    if( res is List ){
-      returnData = (res as List<dynamic>).map( (x) => NumberWordPair.fromJson(x) ).toList();
-    }else if( res is Map ){
-      returnData = <NumberWordPair>[NumberWordPair.fromJson(res as Map<String,dynamic>)];
+    List<dynamic> mData = res['mData'];
+    // ignore: unnecessary_type_check
+    if( mData is List ){
+      returnData = (mData).map( (x) => mLine.fromJson(x) ).toList();
+    }else if( mData is Map ){
+      returnData = <mLine>[mLine.fromJson(mData as Map<String,dynamic>)];
     }else{
       developer.log('ERROR: Unexpected json response type (was not a List or Map).');
       returnData = List.empty();
@@ -227,3 +240,18 @@ Future<List<NumberWordPair>> fetchNumberWordPairs() async {
     throw Exception('Did not receive success status code from request.');
   }
 }
+
+
+void updateLikes(mLine message, int myid, int newLikes) async {
+  // Update the mLikes field of the message object.
+  message.mLikes = newLikes;
+  final response = await http.put(
+    Uri.parse('http://2023sp-team-m.dokku.cse.lehigh.edu/messages/${myid}'),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode(message.toJson()),
+  );
+  if (response.statusCode != 200) {
+    throw Exception('Failed to update like.');
+  }
+}
+
