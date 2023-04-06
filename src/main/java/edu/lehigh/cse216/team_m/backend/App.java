68a6a1b074e8c7c1b2069fcf0f6caf75d9f6f367
David-CN <44127727+jiw324@users.noreply.github.com>
2023-04-06 11:39:20 -0400

    update on backend

--- src/main/java/edu/lehigh/cse216/team_m/backend/App.java
+++ src/main/java/edu/lehigh/cse216/team_m/backend/App.java
@@ -1,20 +1,23 @@
 package edu.lehigh.cse216.team_m.backend;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.ArrayList;
-// Import the Spark package, so that we can make use of the "get" function to 
-// create an HTTP GET route
 import spark.Spark;
-
-// Import Google's JSON library
 import com.google.gson.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
+import org.omg.CORBA.CODESET_INCOMPATIBLE;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import java.util.HashMap;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.json.JsonFactory;
 /**
  * For now, our app creates an HTTP server that can only get and add data.
  */
@@ -30,8 +33,8 @@ public class App {
      * 
      * @return null on failure, otherwise configured database object
      */
-    private static Database getDatabaseConnection() {
-        if (System.getenv("DATABASE_URL") != null) {
+    private static Database getDatabaseConnection(){
+        if( System.getenv("DATABASE_URL") != null ){
             return Database.getDatabase(System.getenv("DATABASE_URL"), DEFAULT_PORT_DB);
         }
 
@@ -41,17 +44,16 @@ public class App {
         String user = env.get("POSTGRES_USER");
         String pass = env.get("POSTGRES_PASS");
         return Database.getDatabase(ip, port, "", user, pass);
-    }
-
+    } 
     /**
-     * Get an integer environment variable if it exists, and otherwise return the
-     * default value.
-     * 
-     * @envar The name of the environment variable to get.
-     * @defaultVal The integer value to use as the default if envar isn't found
-     * 
-     * @returns The best answer we could come up with for a value for envar
-     */
+    * Get an integer environment variable if it exists, and otherwise return the
+    * default value.
+    * 
+    * @param envar The name of the environment variable to get.
+    * @param defaultVal The integer value to use as the default if envar isn't found
+    * 
+    * @return The best answer we could come up with for a value for envar
+    */
     static int getIntFromEnv(String envar, int defaultVal) {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get(envar) != null) {
@@ -60,13 +62,48 @@ public class App {
         return defaultVal;
     }
 
-    public static void main(String[] args) {
+    private static final String CLIENT_ID = "926558226206-ppmn3bk4ckvrtaq6hun9kpi034sde366.apps.googleusercontent.com";
+    // private static final String CLIENT_SECRET = "your-client-secret";
+    // private static final String REDIRECT_URI = "your-redirect-uri";
+    // public static GoogleLogin verify() {
+    //     try {
+    //         String authorizationCode = "your-authorization-code";
+    //         GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(
+    //             HTTP_TRANSPORT,
+    //             JSON_FACTORY,
+    //             CLIENT_ID,
+    //             CLIENT_SECRET,
+    //             authorizationCode,
+    //             REDIRECT_URI).execute();
+    //         String idTokenString = tokenResponse.getIdToken();
+    //         return verifier.verify(idTokenString);
+    //     } 
+    //     catch (Exception e) {
+    //         System.err.println("Exception verifying ID token: " + e.getMessage());
+    //         return null;
+    //     }
+    // }
+    /**
+     * @author David
+     * @version 4/2/2023
+     */
+    public static <GoogleSignInResponse> void main(String[] args) {
+        // Create a new hash map to store the user ID and session key
+        HashMap<String, HashMap<String, String>> usersMap = new HashMap<>();
+        HashMap<String, String> testUser = new HashMap<>();
+        testUser.put("name", "David");
+        testUser.put("email", "jiw324@lehigh.com");
+        testUser.put("gender identity", "Male");
+        testUser.put("sexual orientation", "Heterosexual");
+        testUser.put("note", "Backend dev");
+        usersMap.put("johndoe", testUser);
 
+        System.out.println(usersMap.get("johndoe"));
         // Get the port on which to listen for requests
         Spark.port(getIntFromEnv("PORT", DEFAULT_PORT_SPARK));
 
-        // Set up the location for serving static files. If the STATIC_LOCATION
-        // environment variable is set, we will serve from it. Otherwise, serve
+        // Set up the location for serving static files.  If the STATIC_LOCATION
+        // environment variable is set, we will serve from it.  Otherwise, serve
         // from "/web"
         String static_location_override = System.getenv("STATIC_LOCATION");
         if (static_location_override == null) {
@@ -75,39 +112,34 @@ public class App {
             Spark.staticFiles.externalLocation(static_location_override);
         }
 
-        if ("True".equalsIgnoreCase(System.getenv("CORS_ENABLED"))) {
-            final String acceptCrossOriginRequestsFrom = "*";
-            final String acceptedCrossOriginRoutes = "GET,PUT,POST,DELETE,OPTIONS";
-            final String supportedRequestHeaders = "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin";
-            enableCORS(acceptCrossOriginRequestsFrom, acceptedCrossOriginRoutes, supportedRequestHeaders);
-        }
-
         // gson provides us with a way to turn JSON into objects, and objects
         // into JSON.
         //
         // NB: it must be final, so that it can be accessed from our lambdas
         //
-        // NB: Gson is thread-safe. See
+        // NB: Gson is thread-safe.  See 
         // https://stackoverflow.com/questions/10380835/is-it-ok-to-use-gson-instance-as-a-static-field-in-a-model-bean-reuse
-        final Gson gson = new Gson();
-
-        // dataStore holds all of the data that has been provided via HTTP
-        // requests
-        //
-        // NB: every time we shut down the server, we will lose all data, and
-        // every time we start the server, we'll have an empty dataStore,
-        // with IDs starting over from 0.
+        Gson gson = new Gson();
+        // NB: every time we shut down the server, we will lose all data, and 
+        //     every time we start the server, we'll have an empty dataStore,
+        //     with IDs starting over from 0.
         final DataStore dataStore = new DataStore();
         Database db = getDatabaseConnection();
 
+
+        HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+        JsonFactory JSON_FACTORY = new JacksonFactory();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(HTTP_TRANSPORT, JSON_FACTORY)
+            .setAudience(Collections.singletonList(CLIENT_ID))
+            .build();
         // Set up a route for serving the main page
         Spark.get("/", (req, res) -> {
             res.redirect("/index.html");
             return "";
         });
-        // GET route that returns all message titles and Ids. All we do is get
-        // the data, embed it in a StructuredResponse, turn it into JSON, and
-        // return it. If there's no data, we return "[]", so there's no need
+        // GET route that returns all message titles and Ids.  All we do is get 
+        // the data, embed it in a StructuredResponse, turn it into JSON, and 
+        // return it.  If there's no data, we return "[]", so there's no need 
         // for error handling.
         Spark.get("/messages", (request, response) -> {
             // ensure status 200 OK, with a MIME type of JSON
@@ -117,10 +149,10 @@ public class App {
         });
 
         // GET route that returns everything for a single row in the DataStore.
-        // The ":id" suffix in the first parameter to get() becomes
-        // request.params("id"), so that we can get the requested row ID. If
+        // The ":id" suffix in the first parameter to get() becomes 
+        // request.params("id"), so that we can get the requested row ID.  If 
         // ":id" isn't a number, Spark will reply with a status 500 Internal
-        // Server Error. Otherwise, we have an integer, and the only possible
+        // Server Error.  Otherwise, we have an integer, and the only possible 
         // error is that it doesn't correspond to a row with data.
         Spark.get("/messages/:id", (request, response) -> {
             int idx = Integer.parseInt(request.params("id"));
@@ -135,29 +167,38 @@ public class App {
             }
         });
 
-        // POST route for adding a new element to the DataStore. This will read
-        // JSON from the body of the request, turn it into a SimpleRequest
-        // object, extract the title and message, insert them, and return the
+        Spark.post("/login", (request, response) -> {
+            SimpleRequest req = gson.fromJson(request.body(), SimpleRequest.class);
+            response.status(200);
+            response.type("application/json");
+            String mytoken = req.mMessage;
+            return gson.toJson(new StructuredResponse("ok", mytoken, null));
+        });
+
+        // POST route for adding a new element to the DataStore.  This will read
+        // JSON from the body of the request, turn it into a SimpleRequest 
+        // object, extract the title and message, insert them, and return the 
         // ID of the newly created row.
         Spark.post("/messages", (request, response) -> {
-            // NB: if gson.Json fails, Spark will reply with status 500 Internal
+            // NB: if gson.Json fails, Spark will reply with status 500 Internal 
             // Server Error
             SimpleRequest req = gson.fromJson(request.body(), SimpleRequest.class);
             // ensure status 200 OK, with a MIME type of JSON
             // NB: even on error, we return 200, but with a JSON object that
-            // describes the error.
+            //     describes the error.
             response.status(200);
             response.type("application/json");
             // NB: createEntry checks for null title and message
             int newId = db.insertRow(req.mSubject, req.mMessage);
             if (newId == -1) {
                 return gson.toJson(new StructuredResponse("error", "error performing insertion", null));
-            } else {
+            } 
+            else {
                 return gson.toJson(new StructuredResponse("ok", "" + newId, null));
             }
         });
 
-        // PUT route for updating a row in the DataStore. This is almost
+        // PUT route for updating a row in the DataStore.  This is almost 
         // exactly the same as POST
         Spark.put("/messages/:id", (request, response) -> {
             // If we can't get an ID or can't parse the JSON, Spark will send
@@ -176,7 +217,7 @@ public class App {
         });
 
         // PUT route for liking a row in the DataStore.
-        Spark.put("/messages/:id/like", (request, response) -> {
+        Spark.put ("/messages/:id/like", (request, response) -> {
             // If we can't get an ID or can't parse the JSON, Spark will send
             // a status 500
             int idx = Integer.parseInt(request.params("id"));
@@ -214,47 +255,15 @@ public class App {
             // ensure status 200 OK, with a MIME type of JSON
             response.status(200);
             response.type("application/json");
-            // NB: we won't concern ourselves too much with the quality of the
-            // message sent on a successful delete
+            // NB: we won't concern ourselves too much with the quality of the 
+            //     message sent on a successful delete
             int result = db.deleteRow(idx);
             if (result == -1) {
                 return gson.toJson(new StructuredResponse("error", "unable to delete row " + idx, null));
-            } else {
+            }
+            else {
                 return gson.toJson(new StructuredResponse("ok", null, null));
             }
-        });
-    }
-
-    /**
-     * Set up CORS headers for the OPTIONS verb, and for every response that the
-     * server sends. This only needs to be called once.
-     * 
-     * @param origin  The server that is allowed to send requests to this server
-     * @param methods The allowed HTTP verbs from the above origin
-     * @param headers The headers that can be sent with a request from the above
-     *                origin
-     */
-    private static void enableCORS(String origin, String methods, String headers) {
-        // Create an OPTIONS route that reports the allowed CORS headers and methods
-        Spark.options("/*", (request, response) -> {
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
-            return "OK";
-        });
-
-        // 'before' is a decorator, which will run before any
-        // get/post/put/delete. In our case, it will put three extra CORS
-        // headers into the response
-        Spark.before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", origin);
-            response.header("Access-Control-Request-Method", methods);
-            response.header("Access-Control-Allow-Headers", headers);
         });
     }
 }
