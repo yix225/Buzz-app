0ae9ce8b787dc09ab38730c1c3b747d13af08299
skd225 <skd225@lehigh.edu>
2023-04-10 19:30:55 -0400

    Adding unit tests for DataUser

--- admin-cli/src/test/java/edu/lehigh/cse216/team_m/admin/DatabaseTest.java
+++ admin-cli/src/test/java/edu/lehigh/cse216/team_m/admin/DatabaseTest.java
@@ -6,8 +6,12 @@ import junit.framework.TestSuite;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Formatter.BigDecimalLayoutForm;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * @author Hien Thi
  * @version 3/25/2023
@@ -27,7 +31,16 @@ public class DatabaseTest extends TestCase {
         return new TestSuite(DatabaseTest.class);
     }
 
-    
+    public void testDataUser() {
+        int id = 1231241351; String name = "testname"; 
+        String email = "test@gmail.com"; String genId = "test Gender"; 
+        String sexOtn = "test Orientation"; String note = "test note"; 
+        boolean valid = true; Date created = new Date();
+
+
+    }
+
+
     /**
      * Test to see if inserted id, subject, message and likes match up with the row in the database
      */
